5714671f6b3bc3f323a8384867725b3c7ff13cae
Imp3R4T0R <torocsik.t99@gmail.com>
2023-06-01 18:59:18 +0200

    Refactor

---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Controller\MovieController;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+    $routes->add('movies', '/')->controller([MovieController::class, 'showMovies']);
+    $routes->add('movie', '/addmovie')->controller([MovieController::class, 'addMovie']);
+    $routes->add('editmovie', 'editmovie/{id}')->controller([MovieController::class, 'editMovie'])->methods(['GET', 'POST']);
+    $routes->add('deletemovie', 'deletemovie/{id}')->controller([MovieController::class, 'deleteMovie'])->methods(['GET','DELETE']);
+    $routes->add('fetchmovie', 'fetchmovie')->controller([MovieController::class, 'fetchMovieFromApi']);
+};
